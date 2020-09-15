c2e4ecf3bb35ec704aa6d09c0b76c64365df8a54
rehamalraee <71270985+rehamalraee@users.noreply.github.com>
2020-09-15 09:45:48 +0300

    Update system_window_body.dart

--- lib/models/system_window_body.dart
+++ lib/models/system_window_body.dart
@@ -4,13 +4,15 @@ import 'package:system_alert_window/models/system_window_padding.dart';
 import 'package:system_alert_window/models/system_window_text.dart';
 import 'package:system_alert_window/system_alert_window.dart';
 import 'package:system_alert_window/utils/commons.dart';
+import 'package:video_player/video_player.dart';
 
 class SystemWindowBody {
   List<EachRow> rows;
   SystemWindowPadding padding;
   SystemWindowDecoration decoration;
+  VideoPlayerController _controller;
 
-  SystemWindowBody({this.rows, this.padding, this.decoration});
+  SystemWindowBody({this.rows, this.padding, this.decoration, this._controller});
 
   Map<String, dynamic> getMap() {
     final Map<String, dynamic> map = <String, dynamic>{
@@ -18,7 +20,8 @@ class SystemWindowBody {
           ? null
           : List<dynamic>.from(rows.map((x) => x?.getMap())),
       'padding': padding?.getMap(),
-      'decoration': decoration?.getMap()
+      'decoration': decoration?.getMap(),
+        '_controller':_controller
     };
     return map;
   }
@@ -30,9 +33,9 @@ class EachRow {
   SystemWindowMargin margin;
   ContentGravity gravity;
   SystemWindowDecoration decoration;
-
+  VideoPlayerController _controller;
   EachRow(
-      {this.columns, this.padding, this.margin, this.gravity, this.decoration});
+      {this.columns, this.padding, this.margin, this.gravity, this.decoration,this._controller});
 
   Map<String, dynamic> getMap() {
     final Map<String, dynamic> map = <String, dynamic>{
@@ -42,7 +45,8 @@ class EachRow {
       'padding': padding?.getMap(),
       'margin': margin?.getMap(),
       'gravity': Commons.getContentGravity(gravity),
-      'decoration': decoration?.getMap()
+      'decoration': decoration?.getMap(),
+      '_controller':_controller
     };
     return map;
   }
@@ -53,15 +57,17 @@ class EachColumn {
   SystemWindowPadding padding;
   SystemWindowMargin margin;
   SystemWindowDecoration decoration;
-
-  EachColumn({this.text, this.padding, this.margin, this.decoration});
+  VideoPlayerController _controller;
+  
+  EachColumn({this.text, this.padding, this.margin, this.decoration,this._controller});
 
   Map<String, dynamic> getMap() {
     final Map<String, dynamic> map = <String, dynamic>{
       'text': text?.getMap(),
       'padding': padding?.getMap(),
       'margin': margin?.getMap(),
-      'decoration': decoration?.getMap()
+      'decoration': decoration?.getMap(),
+        '_controller':_controller
     };
     return map;
   }
